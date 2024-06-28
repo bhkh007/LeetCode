05df7a63402cb0c3548c96b3331442919d9f99f6
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-28 12:47:28 +0530

    solved trapping water leetcode problem 42

--- DSA/Sudoku.java
+++ DSA/Sudoku.java
@@ -41,43 +41,6 @@ class Solution {
     }
 }
 
-public class Sudoku {
-    public static void main(String[] args) {
-        // Sample Sudoku puzzle (0 represents an empty cell)
-        char[][] board = {
-            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
-        };
-
-        // Create an instance of Solution
-        Solution solution = new Solution();
-
-        // Solve the Sudoku puzzle
-        solution.solveSudoku(board);
-
-        // Print the solved Sudoku board
-        printBoard(board);
-    }
-
-    // Method to print the Sudoku board
-    private static void printBoard(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                System.out.print(board[row][col] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-
 
 /* 
 
