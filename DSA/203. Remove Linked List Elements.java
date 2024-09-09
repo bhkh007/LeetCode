abb6a4c60e6947b57402816b547fcd8b509f3531
Bhavesh Khandar <162608794+bhkh007@users.noreply.github.com>
2024-09-09 12:11:48 +0530

    Leetcode problem 203

---
+++ DSA/203. Remove Linked List Elements.java
@@ -0,0 +1,60 @@
+
+ // Definition for singly-linked list.
+   class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1,head);
+        ListNode pre = dummy;
+
+        while(pre.next != null){
+            if(pre.next.val != val){
+                pre = pre.next;
+            }
+            else{
+                pre.next = pre.next.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
+
+
+/* 
+                203. Remove Linked List Elements
+
+
+
+                Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+ */
